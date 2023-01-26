5720c64c007675eea69af8ae1ac783afa89ef584
Samuel Fuentes <samuel.fn331@gmail.com>
2023-01-25 23:30:23 -0600

    error correction

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -9,18 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBackground
+        window?.backgroundColor = .blue
         window?.rootViewController = LoginViewController()
         
         return true
     }
+    
+    
+    
     
 }
 
